13494393da226177f34b14accb6bfa79af7dd34a
Adil Tobaa <aatobaa@uchicago.edu>
2018-01-04 15:55:47 -0800

    Implemented Cases A-D

--- DestinationTraining/Controllers/HomeController.cs
+++ DestinationTraining/Controllers/HomeController.cs
@@ -20,10 +20,130 @@ namespace DestinationTraining.Controllers
 
             return View();
         }
-        public ActionResult TestPage(string testparam)
+
+        public ActionResult CaseA(string testparam)
         {
+            var useragent = System.Web.HttpContext.Current.Request.UserAgent;
+            if (useragent.ToLower().Contains("bot"))
+            {
+                Response.StatusCode = 403;
+                Response.StatusDescription = "Bots have been blocked";
+                return Content("403 Forbidden: Bots have been blocked from this page for security reasons.");
+            }
+            else
+            {
+                ViewData["Page"] = "Case A";
+            }
             ViewData["testParam"] = testparam;
+            return View();
+        }
+
+        public ActionResult CaseB(string testparam)
+        {
+            ViewData["Page"] = "Case B";
+            ViewData["testPram"] = testparam;
+            return View();
+        }
+        public ActionResult CaseC(string testparam)
+        {
+            var cookies = System.Web.HttpContext.Current.Request.Cookies;
+            if (cookies.Count > 0)
+            {
+                ViewData["Page"] = "Case C";
+            }
+            else
+            {
+                // Implementation could set cookie here and redirect; leaving out for simplicity. 
+
+                return RedirectToRoute(new { controller = "Home", action = "CaseC" });
+            }
+            ViewData["testParam"] = testparam;
+            return View();
+        }
+        public ActionResult CaseD(string testparam)
+        {
+            if (Helpers.Utils.fBrowserIsMobile())
+            {
+                ViewData["Page"] = "Mobile Detected, redirecting...";
+                return Redirect("m.destinationtraining.azurewebsites.net");
+            }
+            else
+            {
+                ViewData["Page"] = "Case D";
+            }
+
+            ViewData["testParam"] = testparam;
+            return View();
+        }
+        public ActionResult CaseE(string testparam)
+        {
+            var useragent = System.Web.HttpContext.Current.Request.UserAgent;
+            if (useragent.ToLower().Contains("bot"))
+            {
+                Response.StatusCode = 403;
+                Response.StatusDescription = "Bots have been blocked";
+                return Content("403 Forbidden: Bots have been blocked from this page for security reasons.");
+            }
+            else
+            {
+                ViewData["Page"] = "Case E";
+            }
+            ViewData["testParam"] = testparam;
+            return View();
+        }
+        public ActionResult CaseF(string testparam)
+        {
+            var useragent = System.Web.HttpContext.Current.Request.UserAgent;
+            if (useragent.ToLower().Contains("bot"))
+            {
+                Response.StatusCode = 403;
+                Response.StatusDescription = "Bots have been blocked";
+                return Content("403 Forbidden: Bots have been blocked from this page for security reasons.");
+            }
+            else
+            {
+                ViewData["Page"] = "Case F";
+            }
+            ViewData["testParam"] = testparam;
+            return View();
+        }
+        public ActionResult CaseG(string testparam)
+        {
+            var useragent = System.Web.HttpContext.Current.Request.UserAgent;
+            if (useragent.ToLower().Contains("bot"))
+            {
+                Response.StatusCode = 403;
+                Response.StatusDescription = "Bots have been blocked";
+                return Content("403 Forbidden: Bots have been blocked from this page for security reasons.");
+            }
+            else
+            {
+                ViewData["Page"] = "Case G";
+            }
+            ViewData["testParam"] = testparam;
+            return View();
+        }
+        public ActionResult CaseH(string testparam)
+        {
+            var useragent = System.Web.HttpContext.Current.Request.UserAgent;
+            if (useragent.ToLower().Contains("bot"))
+            {
+                Response.StatusCode = 403;
+                Response.StatusDescription = "Bots have been blocked";
+                return Content("403 Forbidden: Bots have been blocked from this page for security reasons.");
+            }
+            else
+            {
+                ViewData["Page"] = "Case H";
+            }
+            ViewData["testParam"] = testparam;
+            return View();
+        }
+
+        public ActionResult TestPage(string testparam)
+        {           
             ViewData["Page"] = "TestPage";
+            ViewData["testParam"] = testparam;
             return View();
         }
     }
